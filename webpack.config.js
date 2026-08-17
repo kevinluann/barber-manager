0bebbd30f1c89cd8a44244eaf4b0190700c8dae3
Kevin Luan <kevinluan.dev@gmail.com>
2026-08-16 22:58:23 -0300

    build: configurar babel-loader
    Configura babel-loader no webpack
    
    - Adiciona regra de processamento para arquivos .js
    - Configura @babel/preset-env como preset de transpilação
    - Exclui node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,15 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
         exclude: '/node_modules/'
+      }, {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
       }
     ]
   }
