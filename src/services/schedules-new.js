692d58a83386a8468ad26d03c2651efb5356f596
Kevin Luan <kevinluan.dev@gmail.com>
2026-08-19 23:57:22 -0300

    feat: adicionar criação de agendamentos
    Adiciona criação de agendamentos na API
    
    - Cria api-config.js com URL base da API
    - Cria schedules-new.js com requisição POST
    - Envia dados do agendamento para json-server

---
+++ src/services/schedules-new.js
@@ -0,0 +1,18 @@
+import { apiConfig } from "./api-config.js"
+
+export async function scheduleNew({ id, name, when }) {
+    try {
+        await fetch(`${apiConfig.baseURL}/schedules`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({id, name, when})
+        })
+
+        alert('Agendamento realizado com sucesso.')
+    } catch (error) {
+        alert('Não foi possível agendar. Tente novamente mais tarde.')
+        console.log(error)
+    }
+}
