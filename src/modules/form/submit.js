2be3d20535b31c4d129f605fb28cada333922ff8
Kevin Luan <kevinluan.dev@gmail.com>
2026-08-21 19:17:40 -0300

    refactor: renomear serviço de agendamento
    Renomeia schedules-new.js para schedule-new.js
    
    - Atualiza import no submit.js

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,6 @@
 import dayjs from "dayjs"
 
-import { scheduleNew } from "../../services/schedules-new.js"
+import { scheduleNew } from "../../services/schedule-new.js"
 
 const form = document.querySelector('form')
 const clientName = document.querySelector('#client')
