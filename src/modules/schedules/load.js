cd8b04f7f7c76e24ed85773aaa70adf0762fdeac
Kevin Luan <kevinluan.dev@gmail.com>
2026-08-21 19:13:28 -0300

    feat: buscar agendamentos por data
    Busca agendamentos do dia na API
    
    - Cria serviço schedule-fetch-by-day.js para buscar agendamentos
    - Filtra agendamentos por data

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,12 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 import { hoursLoad } from "../form/hours-load.js"
 
 const selectedDate = document.querySelector('#date')
 
-export function schedulesDay() {
+export async function schedulesDay() {
     const date = selectedDate.value
+
+    const dailySchedules = await scheduleFetchByDay({ date })
 
     hoursLoad({ date })
 }
